c8c6f2272681020637da96bafa16d00eccd19b5d
alvaropeblas <alvaropeblas@gmail.com>
2024-07-10 12:50:28 +0200

    añade temperatura pc

--- App.js
+++ App.js
@@ -1,8 +1,121 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, createContext, useContext } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
+// Contexto para el estado global
+const AppContext = createContext();
+
+// Componente para el control remoto del ratón
+const ControlRemoto = () => {
+  const { startMoving, stopMoving, sendCommand, sendVolumeCommand } = useContext(AppContext);
+
+  return (
+    <View style={styles.section}>
+      <Text style={styles.sectionTitle}>Controles de Ratón</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPressIn={() => startMoving('move_mouse', 45, 0)}
+          onPressOut={() => stopMoving()}
+        >
+          <Image source={require('./assets/right_arrow.png')} style={styles.icon} />
+          <Text>Mover Derecha</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPressIn={() => startMoving('move_mouse', -45, 0)}
+          onPressOut={() => stopMoving()}
+        >
+          <Image source={require('./assets/left_arrow.png')} style={styles.icon} />
+          <Text>Mover Izquierda</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPressIn={() => startMoving('move_mouse', 0, -45)}
+          onPressOut={() => stopMoving()}
+        >
+          <Image source={require('./assets/up_arrow.png')} style={styles.icon} />
+          <Text>Mover Arriba</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPressIn={() => startMoving('move_mouse', 0, 45)}
+          onPressOut={() => stopMoving()}
+        >
+          <Image source={require('./assets/down_arrow.png')} style={styles.icon} />
+          <Text>Mover Abajo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => sendVolumeCommand('volume_up')}
+        >
+          <Image source={require('./assets/volume_up.png')} style={styles.icon} />
+          <Text>Subir Volumen</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => sendVolumeCommand('volume_down')}
+        >
+          <Image source={require('./assets/volume_down.png')} style={styles.icon} />
+          <Text>Bajar Volumen</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => sendCommand('click_mouse')}
+        >
+          <Text>Hacer Clic</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+// Componente para mostrar la información del sistema
+const SystemInfo = ({ systemInfo }) => (
+  <View style={styles.section}>
+    <Text style={styles.sectionTitle}>Información del Sistema</Text>
+    <Text>Temperatura CPU: {systemInfo.temperature.toFixed(2)}ºC</Text>
+
+    {/* Agregar más información según sea necesario */}
+  </View>
+);
+
+// Componente principal de la aplicación
 export default function App() {
   const [intervalId, setIntervalId] = useState(null);
+  const [systemInfo, setSystemInfo] = useState(null);
+  const [showControlView, setShowControlView] = useState(true);
+  const [showSystemInfoView, setShowSystemInfoView] = useState(false);
+
+  useEffect(() => {
+    fetchSystemInfo();
+  }, []);
+
+
+  const fetchSystemInfo = () => {
+    fetch('http://192.168.1.15:3000/system-info', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Respuesta no exitosa');
+        }
+        return response.json(); // Devuelve el cuerpo de la respuesta como JSON
+      })
+      .then(data => {
+        console.log('Datos del sistema recibidos:', data);
+        // Aquí puedes actualizar el estado de tu componente React Native con los datos recibidos
+        setSystemInfo(data);
+      })
+      .catch(error => {
+        console.log('Error al obtener la información del sistema:', error);
+      });
+  };
+
+
 
   const sendCommand = (command, x, y) => {
     fetch('http://192.168.1.15:3000', {
@@ -18,7 +131,7 @@ export default function App() {
     if (intervalId === null) {
       const id = setInterval(() => {
         sendCommand(command, x, y);
-      }, 100); 
+      }, 100);
       setIntervalId(id);
     }
   };
@@ -30,80 +143,42 @@ export default function App() {
     }
   };
 
+  const sendVolumeCommand = (command) => {
+    sendCommand(command);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Control Remoto</Text>
+    <AppContext.Provider value={{ systemInfo, startMoving, stopMoving, sendCommand, sendVolumeCommand }}>
+      <View style={styles.container}>
 
-      {/* Sección de Controles de Volumen */}
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Controles de Volumen</Text>
-        <View style={styles.buttonContainer}>
+        {/* Menú de Navegación */}
+        <View style={styles.menu}>
           <TouchableOpacity
-            style={styles.button}
-            onPressIn={() => startMoving('volume_up')}
-            onPressOut={() => stopMoving()}
+            style={[styles.menuItem, showControlView && styles.activeMenuItem]}
+            onPress={() => {
+              setShowControlView(true);
+              setShowSystemInfoView(false);
+            }}
           >
-            <Image source={require('./assets/volume_up.png')} style={styles.icon} />
-            <Text>Subir Volumen</Text>
+            <Text style={styles.menuText}>Control Remoto</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={styles.button}
-            onPressIn={() => startMoving('volume_down')}
-            onPressOut={() => stopMoving()}
+            style={[styles.menuItem, showSystemInfoView && styles.activeMenuItem]}
+            onPress={() => {
+              setShowControlView(false);
+              setShowSystemInfoView(true);
+            }}
           >
-            <Image source={require('./assets/volume_down.png')} style={styles.icon} />
-            <Text>Bajar Volumen</Text>
+            <Text style={styles.menuText}>Información del Sistema</Text>
           </TouchableOpacity>
         </View>
-      </View>
 
-      {/* Sección de Controles de Ratón */}
-      <View style={styles.section}>
-        <Text style={styles.sectionTitle}>Controles de Ratón</Text>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPressIn={() => startMoving('move_mouse', 45, 0)}
-            onPressOut={() => stopMoving()}
-          >
-            <Image source={require('./assets/right_arrow.png')} style={styles.icon} />
-            <Text>Mover Derecha</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPressIn={() => startMoving('move_mouse', -45, 0)}
-            onPressOut={() => stopMoving()}
-          >
-            <Image source={require('./assets/left_arrow.png')} style={styles.icon} />
-            <Text>Mover Izquierda</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPressIn={() => startMoving('move_mouse', 0, -45)}
-            onPressOut={() => stopMoving()}
-          >
-            <Image source={require('./assets/up_arrow.png')} style={styles.icon} />
-            <Text>Mover Arriba</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPressIn={() => startMoving('move_mouse', 0, 45)}
-            onPressOut={() => stopMoving()}
-          >
-            <Image source={require('./assets/down_arrow.png')} style={styles.icon} />
-            <Text>Mover Abajo</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => sendCommand('click_mouse')}
-          >
-            <Text>Hacer Clic</Text>
-          </TouchableOpacity>
-        </View>
+        {/* Renderizado condicional de vistas */}
+        {showControlView && <ControlRemoto />}
+        {showSystemInfoView && systemInfo && <SystemInfo systemInfo={systemInfo} />}
+
       </View>
-      {/* Indicador de Estado o Feedback Visual */}
-      {/* Puedes añadir aquí un componente para mostrar el estado actual del PC */}
-    </View>
+    </AppContext.Provider>
   );
 }
 
@@ -118,6 +193,23 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     marginBottom: 20,
+  },
+  menu: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  menuItem: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  menuText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  activeMenuItem: {
+    borderBottomWidth: 2,
+    borderColor: 'blue',
   },
   section: {
     marginBottom: 30,
--- control-remoto-servidor/server.js
+++ control-remoto-servidor/server.js
@@ -1,64 +1,111 @@
 const express = require('express');
 const { exec } = require('child_process');
+const si = require('systeminformation');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:19000', // Reemplaza con el origen de tu aplicación React Native
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+  };
+  
+  app.use(cors(corsOptions));
+app.use(express.json());
 
 // Ruta completa al ejecutable de nircmd.exe
 const nircmdPath = 'C:\\nircmd\\nircmd.exe';
 
-app.use(express.json());
-
 app.post('/', (req, res) => {
-  const { command, x, y } = req.body;
+    const { command, x, y } = req.body;
 
-  switch (command) {
-    case 'volume_up':
-      exec(`${nircmdPath} changesysvolume 2000`, (error) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return;
-        }
-      });
-      break;
-    case 'volume_down':
-      exec(`${nircmdPath} changesysvolume -2000`, (error) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return;
-        }
-      });
-      break;
-    case 'play_pause':
-      exec(`${nircmdPath} sendkeypress media_play_pause`, (error) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return;
-        }
-      });
-      break;
-    case 'move_mouse':
-      exec(`${nircmdPath} movecursor ${x} ${y}`, (error) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return;
-        }
-      });
-      break;
-    case 'click_mouse':
-      exec(`${nircmdPath} sendmouse left click`, (error) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return;
-        }
-      });
-      break;
-    default:
-      console.log('Comando no reconocido');
-  }
+    switch (command) {
+        case 'volume_up':
+            exec(`${nircmdPath} changesysvolume 2000`, (error) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`);
+                    return;
+                }
+            });
+            break;
+        case 'volume_down':
+            exec(`${nircmdPath} changesysvolume -2000`, (error) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`);
+                    return;
+                }
+            });
+            break;
+        case 'play_pause':
+            exec(`${nircmdPath} sendkeypress media_play_pause`, (error) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`);
+                    return;
+                }
+            });
+            break;
+        case 'move_mouse':
+            exec(`${nircmdPath} movecursor ${x} ${y}`, (error) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`);
+                    return;
+                }
+            });
+            break;
+        case 'click_mouse':
+            exec(`${nircmdPath} sendmouse left click`, (error) => {
+                if (error) {
+                    console.error(`Error: ${error.message}`);
+                    return;
+                }
+            });
+            break;
+        default:
+            console.log('Comando no reconocido');
+    }
 
-  res.send('Comando recibido');
+    res.send('Comando recibido');
 });
 
+app.get('/system-info', (req, res) => {
+    // Comando para obtener la temperatura de la CPU usando wmic
+    const command = 'wmic /namespace:\\\\root\\wmi PATH MSAcpi_ThermalZoneTemperature get CurrentTemperature';
+
+    // Ejecutar el comando
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error('Error al ejecutar el comando:', error);
+            res.status(500).json({ error: 'Error al obtener la temperatura' });
+            return;
+        }
+        
+        // Verificar si stdout contiene la información esperada
+        if (!stdout || typeof stdout !== 'string') {
+            console.error('Error: La salida del comando no es válida');
+            res.status(500).json({ error: 'No se pudo obtener la temperatura' });
+            return;
+        }
+
+        // Obtener la temperatura de la salida del comando
+        const temperatureMatch = stdout.match(/\d+/); // Buscar un número en la salida
+        if (!temperatureMatch) {
+            console.error('Error: No se pudo extraer la temperatura de la salida del comando');
+            res.status(500).json({ error: 'No se pudo obtener la temperatura' });
+            return;
+        }
+
+        // Convertir la temperatura a grados Celsius
+        const temperatureRaw = parseInt(temperatureMatch[0]);
+        const celsiusTemp = (temperatureRaw / 10) - 273.15;
+
+        // Enviar la temperatura como respuesta JSON
+        res.json({ temperature: celsiusTemp });
+    });
+});
+
+
+
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
